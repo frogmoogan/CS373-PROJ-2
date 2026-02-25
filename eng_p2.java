21b7e3b5ebc992e3cb7beb87e8c42df4e7492cc9
frogmoogan <meng5@binghamton.edu>
2026-02-25 14:21:06 -0500

    input read good

---
+++ eng_p2.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class eng_p2 {
+    public static void main(String[] args){
+
+        String directions = "";
+        String input = "";
+
+       directions = args[0];
+       input = args[1];
+
+       System.out.println("directions " + directions + " input " + input );
+        
+    }
+
+}
